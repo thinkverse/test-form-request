046d8a69a92e52643ffe1471a069158be8bbca58
thinkverse <hallberg.kim@gmail.com>
2022-07-29 17:00:04 +0200

    change controller to PostRequest

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,17 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Api\PostRequest;
 
 class PostController extends Controller
 {
     /**
      * Handle the incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Http\Requests\Api\PostRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke(PostRequest $request)
     {
         //
     }
