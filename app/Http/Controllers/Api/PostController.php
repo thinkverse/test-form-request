de64ddce5775792877fbda582705dd7c6532be43
thinkverse <hallberg.kim@gmail.com>
2022-07-29 17:01:04 +0200

    add JsonReponse to PostController

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\PostRequest;
+use Illuminate\Http\JsonResponse;
 
 class PostController extends Controller
 {
@@ -11,10 +12,13 @@ class PostController extends Controller
      * Handle the incoming request.
      *
      * @param  \App\Http\Requests\Api\PostRequest  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function __invoke(PostRequest $request)
+    public function __invoke(PostRequest $request): JsonResponse
     {
-        //
+        $validated = $request->safe();
+
+        return response()
+            ->json($validated);
     }
 }
